7ee8490e61936456cadc43c4f58ab7388dd0b16c
HarukaYamamoto0 <harukayamamotodev@gmail.com>
2026-01-13 22:43:08 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+  // This is my three program in C
+
+  /*
+  This is 
+  also
+  a
+  comment
+
+  As long as it's within the comment section
+  
+  everything else will be ignored.
+
+  "To have never suffered is to have never been blessed." - Edgar Allan Poe
+  */
+  
+  printf("Hello, World!\n");
+  printf("My name is Albert\n");
+  return 0;
+}
