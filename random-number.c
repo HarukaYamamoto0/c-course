0364a80667ab607396fa31a33aa2e7c71736fbea
HarukaYamamoto0 <harukayamamotodev@gmail.com>
2026-01-21 02:29:04 -0300

    step: random numbers

---
+++ random-number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+  int max = 50;
+  int min = 0;
+
+  srand(time(NULL));
+  int random = (rand() % (max - min + 1)) + min;
+
+  printf("%d\n", random);
+  return 0;
+}
